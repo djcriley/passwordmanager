518d681404dd86458b4e4d753a83f0be4d6acf66
Cooper Riley <43011353+djcriley@users.noreply.github.com>
2023-02-21 16:14:50 -0600

    User sign in works now. modified to MVC structure

--- src/main/java/com/cooper/passwordmanager/PasswordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cooper.passwordmanager;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordRepository extends JpaRepository<Password, Long> {
-}
--- src/main/java/com/cooper/passwordmanager/PasswordmanagerApplication.java
+++ src/main/java/com/cooper/passwordmanager/PasswordmanagerApplication.java
@@ -1,13 +1,32 @@
 package com.cooper.passwordmanager;
 
+import com.cooper.passwordmanager.model.Password;
+import com.cooper.passwordmanager.repository.PasswordRepository;
+import com.cooper.passwordmanager.service.PasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PasswordmanagerApplication {
+public class PasswordmanagerApplication implements CommandLineRunner {
+
+	@Autowired
+	PasswordRepository passwordRepository;
+	@Autowired
+	PasswordService passwordService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PasswordmanagerApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+//		passwordRepository.save(new Password(".com", "username", "password"));
+
+//		passwordService.savePassword(new Password(".net", "username", "password"));
+	}
 }
